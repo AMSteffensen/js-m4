66272976cb97de66fde267a6484a455258c38d86
amsteffensen <andreassteffensen@Andreass-MacBook-Air.local>
2018-09-30 19:30:43 +0200

    adding styles

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -10,36 +10,28 @@ var section = document.querySelector('section');
 
 request.onload = function() {
     var userData = request.response;
-    populateCards(userData);
     showCards(userData);
-  }
-
-  function populateCards(jsonObj) {
-    var myCard = document.createElement('div');
-    myCard.textContent = jsonObj['id'];
-    header.appendChild(myCard);
-    
-
- /*    // loop through json and populate data
-    for(var i = 0; i < jsonObj.length; i++) {
-        var obj = jsonObj[i];
-    
-        console.log(obj.userId);
-        console.log(obj.id);
-        console.log(obj.title);
-        console.log(obj.completed);
-
-        var myPara = document.createElement('p');
-        myPara.textContent = 'User ID: ' + obj['userId'] + ' // ID: ' + obj['id'];
-        div.setAttribute('class', 'note');
-        header.appendChild(myPara);
-    } */
+    addStyle(userData);
   }
 
 
-  function showCards(jsonObj) {
+  function addStyle(jsonObj){ 
+    var cards = document.querySelectorAll('article');
+    var section = document.querySelectorAll('section');
+    
+
   
-        
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].style.textAlign = 'center';
+        cards[i].style.color = 'black';
+        cards[i].style.boxShadow = '0px 10px';
+        cards[i].style.backgroundColor = 'white';
+    }
+  }
+
+
+
+  function showCards(jsonObj) {   
     for (var i = 0; i < jsonObj.length; i++) {
     
     var cards = jsonObj[i];
@@ -58,19 +50,13 @@ request.onload = function() {
       myPara3.textContent = 'Title: ' + jsonObj[i].title;
       myPara4.textContent = 'Completed: ' + jsonObj[i].completed;
           
-     
       myArticle.appendChild(myH2);
-      
       myArticle.appendChild(myPara1);
       myArticle.appendChild(myPara2);
       myArticle.appendChild(myPara3);
       myArticle.appendChild(myList);
   
       section.appendChild(myArticle);
-
-
-      //document.body.appendChild(div);
-
-
+     
     }
   }
